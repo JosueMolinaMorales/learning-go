abcba701d3807f66a9390500492f79e0510b2b34
Josue Morales <molinajosue92@hotmail.com>
2023-06-01 18:16:19 -0400

    add gif creation

--- go-book/ch1/echo1.go
+++ go-book/ch1/echo1.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
 )
 
 func main() {
-	echo1()
-	echo2()
-	echo3()
+	// echo1()
+	// echo2()
+	// echo3()
+	// dup1()
+	// dup2()
+	Lissajous(os.Stdout)
 }
 
 func echo1() {
@@ -37,4 +41,52 @@ func echo2() {
 
 func echo3() {
 	fmt.Println("echo 3: " + strings.Join(os.Args[1:], " "))
+}
+
+func dup1() {
+	// make(map[string]int) --> Makes a map with the type of key being string
+	// and the type of the value being an int
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignore potental errors from input.err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func dup2() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	fmt.Println(files)
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignore potental errors from input.err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
 }
