8b5105aea2aee5a969ef5da81ac4743b567dee52
Josue Morales <molinajosue92@hotmail.com>
2023-06-02 18:03:50 -0400

    finish rock paper scissors

--- go-book/projects/rps/main.go
+++ go-book/projects/rps/main.go
@@ -11,64 +11,84 @@ import (
 )
 
 func main() {
-	// Rock paper scissors
-	choices := map[int]string{1: "Rock", 2: "Paper", 3: "Scissors"}
-	compChoice := choices[rand.Intn(len(choices))+1]
-	// Get the choice of the user
-	fmt.Println("Please enter one of the following choices:")
-	fmt.Println("1. Rock")
-	fmt.Println("2. Paper")
-	fmt.Println("3. Scissor")
-	reader := bufio.NewReader(os.Stdin)
-	pChoice, err := reader.ReadString('\n')
+	playerWins := 0
+	compWins := 0
+	keepPlaying := ""
+	for keepPlaying != "n" {
+		// Rock paper scissors
+		choices := map[int]string{1: "Rock", 2: "Paper", 3: "Scissors"}
+		compChoice := choices[rand.Intn(len(choices))+1]
+		// Get the choice of the user
+		fmt.Println("Please enter one of the following choices:")
+		fmt.Println("1. Rock")
+		fmt.Println("2. Paper")
+		fmt.Println("3. Scissor")
+		fmt.Print(">>>>>>>>> ")
+		reader := bufio.NewReader(os.Stdin)
+		pChoice, err := reader.ReadString('\n')
 
-	if err != nil {
-		log.Fatalf("Could not read user's input, %v", err)
-		return
+		if err != nil {
+			log.Fatalf("Could not read user's input, %v", err)
+			return
+		}
+
+		// Check the user's input
+		pChoice = s.Trim(pChoice, " \n\t\r")
+		piChoice, err := strconv.Atoi(pChoice)
+
+		if err != nil {
+			log.Fatalf("[ERROR] %s is not a number", pChoice)
+			return
+		}
+		if piChoice < 1 || piChoice > 3 {
+			log.Fatalf("[ERROR] %d is not a valid number", piChoice)
+			return
+		}
+
+		pChoice = choices[piChoice]
+		fmt.Printf("Player's choice is: %s\n", pChoice)
+		fmt.Printf("Computer's choice is: %s\n", compChoice)
+		// Check for winner
+		if pChoice == compChoice {
+			// Draw
+			fmt.Println("It's a draw!")
+		} else if pChoice == "Rock" {
+			if compChoice == "Paper" {
+				fmt.Println("Computer wins!")
+				compWins++
+			}
+			if compChoice == "Scissors" {
+				fmt.Println("Player wins!")
+				playerWins++
+			}
+		} else if pChoice == "Paper" {
+			if compChoice == "Rock" {
+				fmt.Println("Player wins!")
+				playerWins++
+			}
+			if compChoice == "Scissors" {
+				fmt.Println("Computer wins!")
+				compWins++
+			}
+		} else if pChoice == "Scissors" {
+			if compChoice == "Rock" {
+				fmt.Println("Computer wins!")
+				compWins++
+			}
+			if compChoice == "Paper" {
+				fmt.Println("Player wins!")
+				playerWins++
+			}
+		}
+		fmt.Print("Play again? (y/n) >>>>> ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatalf("Could not read the user's input, %v", err)
+		}
+		keepPlaying = s.Trim(input, " \n\t\r")
 	}
-
-	// Check the user's input
-	pChoice = s.Trim(pChoice, " \n\t\r")
-	piChoice, err := strconv.Atoi(pChoice)
-
-	if err != nil {
-		log.Fatalf("[ERROR] %s is not a number", pChoice)
-		return
-	}
-	if piChoice < 1 || piChoice > 3 {
-		log.Fatalf("[ERROR] %d is not a valid number", piChoice)
-		return
-	}
-
-	pChoice = choices[piChoice]
-	fmt.Printf("Player's choice is: %s\n", pChoice)
-	fmt.Printf("Computer's choice is: %s\n", compChoice)
-	// Check for winner
-	if pChoice == compChoice {
-		// Draw
-		fmt.Println("It's a draw!")
-	} else if pChoice == "Rock" {
-		if compChoice == "Paper" {
-			fmt.Println("Computer wins!")
-		}
-		if compChoice == "Scissors" {
-			fmt.Println("Player wins!")
-		}
-	} else if pChoice == "Paper" {
-		if compChoice == "Rock" {
-			fmt.Println("Player wins!")
-		}
-		if compChoice == "Scissors" {
-			fmt.Println("Computer wins!")
-		}
-	} else if pChoice == "Scissors" {
-		if compChoice == "Rock" {
-			fmt.Println("Computer wins!")
-		}
-		if compChoice == "Paper" {
-			fmt.Println("Player wins!")
-		}
-	}
-
 	fmt.Println("Thanks for playing!")
+	fmt.Println("============RESULTS==============")
+	fmt.Println("Player wins: ", playerWins)
+	fmt.Println("Computer wins: ", compWins)
 }
